66deb48edcbda03554477b024d3c04a6a4251939
Wouter van den Heuvel <exo_00@hotmail.com>
2020-05-14 20:27:35 +0200

    Updates

---
+++ precache-manifest.c3cff5329b90b937fffeb11f41b74b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283efc8cd8cf10d598efa396c062b17d",
+    "url": "/teaching-aid/index.html"
+  },
+  {
+    "revision": "4b51753e8bfad45bf9de",
+    "url": "/teaching-aid/static/css/main.d4a92729.chunk.css"
+  },
+  {
+    "revision": "a7ab787585b978a71da4",
+    "url": "/teaching-aid/static/js/2.5310365c.chunk.js"
+  },
+  {
+    "revision": "c521efc2c124b50629d0c03990259819",
+    "url": "/teaching-aid/static/js/2.5310365c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b51753e8bfad45bf9de",
+    "url": "/teaching-aid/static/js/main.28ee7122.chunk.js"
+  },
+  {
+    "revision": "ed96c88a74ceca3a517c",
+    "url": "/teaching-aid/static/js/runtime-main.6397e33b.js"
+  }
+]);
